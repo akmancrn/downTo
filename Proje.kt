328694e7449365079c003e55baf7e57e4e599d77
Ceren Akman <42838160+akmancrn@users.noreply.github.com>
2020-01-15 15:51:26 +0300

    Create Proje.kt

---
+++ Proje.kt
@@ -0,0 +1,15 @@
+package com.example.myapplication
+
+
+import java.io.PrintStream
+import java.util.Scanner
+import java.util.*
+
+fun main (args : Array<String>)
+{
+   var a :Int
+   for(a in 10.downTo(1)){
+      println(a)
+   }
+
+}
